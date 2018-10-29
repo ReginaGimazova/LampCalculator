da72b1df45e8cc7f35b61db247239fe578b02810
Regina <reginagimazova98@gmail.com>
2018-10-29 17:30:29 +0300

    calculator

---
+++ scripts/sub.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rega
+ * Date: 03.10.18
+ * Time: 14:15
+ */
+
+function operation($num1, $num2, $operator){
+    switch ($operator)
+    {
+        case '-':
+            return $num1 - $num2;
+            break;
+
+        default:
+            return 'operator not found';
+    }
+}
+
+$result = 0;
+
+if (isset($_POST['submit'])){
+    $result = operation($_POST['num1'], $_POST['num2'], $_POST['oper']);
+}
+
+require '../templates/sub.tmpl.php';
---
+++ scripts/sum.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rega
+ * Date: 03.10.18
+ * Time: 14:08
+ */
+
+
+function operation($num1, $num2, $operator){
+    switch ($operator)
+    {
+        case '+':
+            return $num1 + $num2;
+            break;
+
+        default:
+            return 'operator not found';
+    }
+}
+
+$result = 0;
+
+if (isset($_POST['submit'])){
+    $result = operation($_POST['num1'], $_POST['num2'], $_POST['oper']);
+}
+
+require '../templates/sum.tmpl.php';
